5438b7fcab5d3eb3912759bcbd1ca2c1273c17fc
AWDANIL <aanil22@tbc.edu.np>
2024-12-16 14:40:34 +0545

    Resolve merge conflicts

--- Final_Comp_2-AWE/routes/web.php
+++ Final_Comp_2-AWE/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PlayerController;
 use Illuminate\Support\Facades\Route;
+
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,5 +19,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::resource('players', PlayerController::class);
 
 require __DIR__.'/auth.php';
